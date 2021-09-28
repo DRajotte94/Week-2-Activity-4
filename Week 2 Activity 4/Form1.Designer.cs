bb06841cd6c2f985e8a51a8c223af3898c2fe1ce
drajo <drajo@DESKTOP-LSTFBT7>
2021-09-28 09:38:13 -0700

    Add project files.

---
+++ Week 2 Activity 4/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿
+namespace Week_2_Activity_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.inputBox = new System.Windows.Forms.TextBox();
+            this.submitButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.minuteBox = new System.Windows.Forms.TextBox();
+            this.hourBox = new System.Windows.Forms.TextBox();
+            this.dayBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(234, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Input a number of seconds";
+            // 
+            // inputBox
+            // 
+            this.inputBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputBox.Location = new System.Drawing.Point(261, 37);
+            this.inputBox.Name = "inputBox";
+            this.inputBox.Size = new System.Drawing.Size(119, 29);
+            this.inputBox.TabIndex = 1;
+            // 
+            // submitButton
+            // 
+            this.submitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.submitButton.Location = new System.Drawing.Point(121, 283);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(175, 67);
+            this.submitButton.TabIndex = 2;
+            this.submitButton.Text = "Submit";
+            this.submitButton.UseVisualStyleBackColor = true;
+            this.submitButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "There are:";
+            // 
+            // minuteBox
+            // 
+            this.minuteBox.Enabled = false;
+            this.minuteBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.minuteBox.Location = new System.Drawing.Point(100, 109);
+            this.minuteBox.Name = "minuteBox";
+            this.minuteBox.Size = new System.Drawing.Size(213, 26);
+            this.minuteBox.TabIndex = 4;
+            // 
+            // hourBox
+            // 
+            this.hourBox.Enabled = false;
+            this.hourBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hourBox.Location = new System.Drawing.Point(100, 162);
+            this.hourBox.Name = "hourBox";
+            this.hourBox.Size = new System.Drawing.Size(213, 26);
+            this.hourBox.TabIndex = 5;
+            // 
+            // dayBox
+            // 
+            this.dayBox.Enabled = false;
+            this.dayBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dayBox.Location = new System.Drawing.Point(100, 214);
+            this.dayBox.Name = "dayBox";
+            this.dayBox.Size = new System.Drawing.Size(213, 26);
+            this.dayBox.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(195, 138);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(23, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "or";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(195, 191);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(23, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "or";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(405, 395);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dayBox);
+            this.Controls.Add(this.hourBox);
+            this.Controls.Add(this.minuteBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.submitButton);
+            this.Controls.Add(this.inputBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox inputBox;
+        private System.Windows.Forms.Button submitButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox minuteBox;
+        private System.Windows.Forms.TextBox hourBox;
+        private System.Windows.Forms.TextBox dayBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Week 2 Activity 4/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Week_2_Activity_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // variables
+            int seconds, minutes, hours, days;
+
+            // converts input from string to integer
+            if (int.TryParse(inputBox.Text, out seconds))
+            {
+                if (seconds >= 60) // counts and displays minutes
+                {
+                    minutes = seconds / 60;
+                    minuteBox.Text = minutes.ToString() + " minute(s)";
+                }
+                else // resets to 0
+                {
+                    minutes = 0;
+                    minuteBox.Text = minutes.ToString() + " minute(s)";
+                }
+
+                if (seconds >= 3600) // counts and displays hours
+                {
+                    hours = seconds / 3600;
+                    hourBox.Text = hours.ToString() + " hour(s)";
+                }
+                else // resets to 0
+                {
+                    hours = 0;
+                    hourBox.Text = hours.ToString() + " hour(s)";
+                }
+
+                if (seconds >= 86400) // counts and displays days
+                {
+                    days = seconds / 86400;
+                    dayBox.Text = days.ToString() + " day(s)";
+                }
+                else // resets to 0
+                {
+                    days = 0;
+                    dayBox.Text = days.ToString() + " day(s)";
+                }
+
+            } 
+            else
+            {
+                // if unable to convert input to integer, show error message
+                MessageBox.Show("Please enter a number");
+            }
+        }
+    }
+}
